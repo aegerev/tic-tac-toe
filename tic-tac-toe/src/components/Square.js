8fd228bbc979561fd77390dbce706b0f5c603faa
alex <alexandersegerev@gmail.com>
2024-09-01 14:58:21 -0400

    Modified the Square.js file

--- tic-tac-toe/src/components/Square.js
+++ tic-tac-toe/src/components/Square.js
@@ -2,7 +2,19 @@ import React from 'react'
 import '../App.css'
 
 function Square(props) {
-    return <div className="squareWrapper">{props.value}</div>;
+    return (
+        <div className={`squareWrapper ${
+            props.value !== "+"
+                ? props.value === "X"
+                    ? "redBg"
+                    : "blueBg"
+                : ""
+        }`}
+        >
+        {props.value}
+    </div>
+
+    )
 }
 
 export default Square;
